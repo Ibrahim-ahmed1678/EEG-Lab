2f6d657c1343b4fba7a9deddd40e1254829db9b9
Simon Aks <134775953+simonaks05@users.noreply.github.com>
2026-02-02 02:06:23 -0800

    Delete split_by_recording.m
    first time using github, will reupload in correct folder format

--- split_by_recording.m
+++
@@ -1,9 +0,0 @@
-function splits = split_by_recording(meta, test_recording_id)
-
-is_test  = meta.recording_id == test_recording_id;
-is_train = ~is_test;
-
-splits.train_idx = find(is_train);
-splits.test_idx  = find(is_test);
-
-end
